34096c5dd018298c30c8531c280170c55e1ff303
haydendav <davishayden99@gmail.com>
2025-02-09 20:30:30 -0500

    delete everything

--- src/main/java/com/chess/utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.chess.utils;
-
-public class Utils {
-    public static String toChessNotation(int x, int y) {
-        char file = (char) ('a' + y);
-        int rank = 8 - x;
-        return "" + file + rank;
-    }
-}
